53bc184ce1cfee426ff98cc6e9ce22c8fde34cbc
xunil@swganh.org <sewell@ironman.(none)>
2010-05-04 00:45:40 -0400

    Added too many zeros.  Now returns proper system time in ms

--- src/Utils/clock.cpp
+++ src/Utils/clock.cpp
@@ -100,7 +100,7 @@ uint64 Clock::getLocalTime() const
 #else
 	struct timeval tv;
 	gettimeofday(&tv, NULL);
-	return (tv.tv_sec*1000000)+(tv.tv_usec / 1000);
+	return (tv.tv_sec*1000)+(tv.tv_usec / 1000);
 #endif
 }
 
